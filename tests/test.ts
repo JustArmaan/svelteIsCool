be12a94e3e813b2c2b2a807e2bf3e37f27beeff8
JustArmaan <singharmaanbrar@gmail.com>
2024-09-26 23:28:41 -0700

    create new svelte route

--- tests/test.ts
+++ tests/test.ts
@@ -4,3 +4,8 @@ test('home page has expected h1', async ({ page }) => {
 	await page.goto('/');
 	await expect(page.locator('h1')).toBeVisible();
 });
+
+test('home page has expected title', async ({ page }) => {
+	await page.goto('/svelte-is-cool');
+	expect(await page.title()).toBe('Svelte is cool');
+}
